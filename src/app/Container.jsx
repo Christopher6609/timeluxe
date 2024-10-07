e8ff9faf765c5c49ef0ab1689459919f38a4c92d
Christopher <odeniyichristopher7@gmail.com>
2024-10-07 23:47:58 +0100

    application header section

---
+++ src/app/Container.jsx
@@ -0,0 +1,11 @@
+
+
+const Container = ({children}) => {
+    return(
+        <main className="max-w-[1440px] mx-auto px-[4.81rem]">
+            {children}
+        </main>
+    )
+}
+
+export default Container;
